e78f4a223282c76bf9bca50a6acf7f4ea2d3b0ec
salojan <salojan@7612dc39-c97b-0410-bcd9-8af2d848e4b8>
2010-02-10 08:05:33 +0000

    Sanitized session expiration.

--- trunk/skin/cgi-bin/logout.pl
+++ trunk/skin/cgi-bin/logout.pl
@@ -12,18 +12,25 @@ require "access.lib";
 
 
 &headPrepare();
-
-my @c = ();
-my @r = ();
-
-@r = &sendCommand({ command => "deleteTempFaction", item => "", domain => "", param => "", option => "" });
-@r = &sendCommand({ command => "tryLogout", item => "", domain => "", param => "", option => "" });
-
-$c[0] = &headCookieSet({ name => "tellu_cid", value => "" });
-$c[1] = &headCookieSet({ name => "tellu_uid", value => "" });
-
-$PAGE .= "<p>If page is not redirected automatically, please click <a href=\"login.pl\">here</a>.</p>";
-
-&htmlPage({ redirect => "login.pl", title => $WINDOW_TITLE, script => "", header => "", content => $PAGE, slices => $MENU, cookie => \@c });
+&doLogout();
 
 exit(0);
+
+
+
+sub doLogout {
+	my ($arg) = @_;
+
+	my @c = ();
+	my @r = ();
+
+	@r = &sendCommand({ command => "deleteTempFaction", item => "", domain => "", param => "", option => "" });
+	@r = &sendCommand({ command => "tryLogout", item => "", domain => "", param => "", option => "" });
+
+	$c[0] = &headCookieSet({ name => "tellu_cid", value => "" });
+	$c[1] = &headCookieSet({ name => "tellu_uid", value => "" });
+
+	$PAGE .= "<p>If page is not redirected automatically, please click <a href=\"login.pl\">here</a>.</p>";
+
+	&htmlPage({ redirect => "login.pl", title => $WINDOW_TITLE, script => "", header => "", content => $PAGE, slices => $MENU, cookie => \@c });
+}
