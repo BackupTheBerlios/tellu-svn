00f3fbdd9c791e603c5464daafa63758cf716b6e
salojan <salojan@7612dc39-c97b-0410-bcd9-8af2d848e4b8>
2010-07-15 15:55:43 +0000

    Better support for reading disk info on *BSD.

--- trunk/agent/tellu-agent-unix-3.0.0/disk.c
+++ trunk/agent/tellu-agent-unix-3.0.0/disk.c
@@ -2,10 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/statvfs.h>
 
 #if defined(__linux__)
 #include <mntent.h>
+#elif defined(__FreeBSD__)
+#include <sys/param.h>
+#include <sys/ucred.h>
+#include <sys/mount.h>
+#elif defined(__OpenBSD__)
+#include <sys/param.h>
+#include <sys/mount.h>
 #endif
 
 #include "declarations.h"
@@ -15,7 +23,7 @@
 char *diskGetMounted(struct paramInfo * pi) {
 #if defined(__linux__)
 	int k;
-	unsigned long sysMultiplier;
+	unsigned long p;
 	unsigned long long sysSize, sysFree, sysSerial;
 
 	char *newBuffer, *tmpBuffer;
@@ -58,14 +66,14 @@ char *diskGetMounted(struct paramInfo * pi) {
 		memset(&newStat, 0, sizeof(newStat));
 
 		if(statvfs(newEntry->mnt_dir, &newStat) == 0) {
-			sysMultiplier = 0;
+			p = 0;
 
-			if(newStat.f_bsize > 0) {
-				sysMultiplier = newStat.f_bsize;
+			if(newStat.f_frsize > 0) {
+				p = newStat.f_frsize;
 			}
 
-			sysSize = (newStat.f_blocks * sysMultiplier) / 1000;
-			sysFree = (newStat.f_bfree * sysMultiplier) / 1000;
+			sysSize = (newStat.f_blocks * p) / 1000;
+			sysFree = (newStat.f_bfree * p) / 1000;
 			sysSerial = newStat.f_fsid;
 		}
 
@@ -99,33 +107,70 @@ char *diskGetMounted(struct paramInfo * pi) {
 	newBuffer[k] = 0;
 
 	return(newBuffer);
-#elif defined(__NetBSD__)
+#elif defined(__FreeBSD__) || (__NetBSD__) || (__OpenBSD__)
 	int i, j, k, l, m;
-	unsigned long f, g;
-	unsigned long sysMultiplier;
+	unsigned long g;
 	unsigned long long sysSize, sysFree, sysSerial;
-
+#if defined(__FreeBSD__)
+	uint64_t f, p;
+#elif defined(__NetBSD__)
+	unsigned long f, p;
+#elif defined(__OpenBSD__)
+	u_int32_t f, p;
+#endif
 	char *newBuffer, *tmpBuffer;
 	char newOptions[CONFIG_SPACE_SIZE];
 
 	size_t newBuflen, newBufcur, newBuffil;
+#if defined(__FreeBSD__)
+	struct statfs *newStat;
 
-	struct statvfs *newStat;
-
-	unsigned long newFlags[] = {
-		0x00000001, 0x00000002, 0x00000004, 0x00000008, 0x00000010, 0x00000020, 0x00000040, 0x00008000, 0x00100000, 0x02000000,
-		0x04000000, 0x20000000, 0x40000000, 0x80000000, 0x00000080, 0x00000100, 0x00000200, 0x00000400, 0x00000800, 0x08000000,
-		0x10000000, 0x00001000, 0x00002000, 0x00004000, 0x00010000, 0x00040000, 0x00080000, 0x00400000,
+	uint64_t newFlags[] = {
+		MNT_RDONLY, MNT_SYNCHRONOUS, MNT_NOEXEC, MNT_NOSUID, MNT_UNION, MNT_ASYNC, MNT_SUIDDIR, MNT_SOFTDEP, MNT_NOSYMFOLLOW,
+		MNT_GJOURNAL, MNT_MULTILABEL, MNT_ACLS, MNT_NOATIME, MNT_NOCLUSTERR, MNT_NOCLUSTERW, MNT_EXRDONLY, MNT_EXPORTED,
+		MNT_DEFEXPORTED, MNT_EXPORTANON, MNT_EXKERB, MNT_EXPUBLIC, MNT_LOCAL, MNT_QUOTA, MNT_ROOTFS, MNT_USER, MNT_IGNORE,
 		-1
 	};
 
-	char *newClags[] = {
+	const char *newClags[] = {
+		"read-only", "synchronous", "noexec", "nosuid", "union", "asynchronous", "suiddir", "soft dependencies", "nosymlinks",
+		"gjournal", "multilabel", "acls", "noatime", "noclusterr", "noclusterw", "exported read-only", "NFS exported",
+		"exported to the world", "anon uid mapping", "kerberos uid mapping", "WebNFS exports", "local", "with quotas",
+		"root file system", "user", "ignore"
+	};
+#elif defined(__NetBSD__)
+	struct statvfs *newStat;
+
+	unsigned long newFlags[] = {
+		MNT_RDONLY, MNT_SYNCHRONOUS, MNT_NOEXEC, MNT_NOSUID, MNT_NODEV, MNT_UNION, MNT_ASYNC, MNT_NOCOREDUMP, MNT_IGNORE, MNT_LOG,
+		MNT_NOATIME, MNT_SYMPERM, MNT_NODEVMTIME, MNT_SOFTDEP, MNT_EXRDONLY, MNT_EXPORTED, MNT_DEFEXPORTED, MNT_EXPORTANON,
+		MNT_EXKERB, MNT_EXNORESPORT, MNT_EXPUBLIC, MNT_LOCAL, MNT_QUOTA, MNT_ROOTFS, MNT_UPDATE, MNT_RELOAD, MNT_FORCE, MNT_GETARGS,
+		-1
+	};
+
+	const char *newClags[] = {
 		"read-only", "synchronous", "noexec", "nosuid", "nodev", "union", "asynchronous", "nocoredump", "hidden", "log",
 		"noatime", "symperm", "nodevmtime", "soft dependencies", "exported read-only", "NFS exported", "exported to the world",
 		"anon uid mapping", "kerberos uid mapping", "on-reserved ports", "WebNFS exports", "local", "with quotas", "root file system",
 		"being updated", "reload filesystem data", "force unmount or readonly change", "retrieve mount arguments"
 	};
+#elif defined(__OpenBSD__)
+	struct statfs *newStat;
 
+	u_int32_t newFlags[] = {
+		MNT_RDONLY, MNT_SYNCHRONOUS, MNT_NOEXEC, MNT_NOSUID, MNT_NODEV, MNT_ASYNC, MNT_EXRDONLY, MNT_EXPORTED, MNT_DEFEXPORTED,
+		MNT_EXPORTANON, MNT_EXKERB, MNT_LOCAL, MNT_QUOTA, MNT_ROOTFS, MNT_NOATIME, MNT_UPDATE, MNT_DELEXPORT, MNT_RELOAD,
+		MNT_FORCE, MNT_WANTRDWR, MNT_SOFTDEP, MNT_DOOMED,
+		-1
+	};
+
+	const char *newClags[] = {
+		"read-only", "synchronous", "noexec", "nosuid", "nodev", "asynchronous", "exported read-only", "NFS exported",
+		"exported to the world", "anon uid mapping", "kerberos uid mapping", "local", "with quotas", "root file system",
+		"noatime", "being updated", "delete export", "reload filesystem data", "force unmount or readonly change", "want rdrw",
+		"soft dependencies", "doomed"
+	};
+#endif
 	newBuffil = 0;
 	newBuflen = DATA_BLOCK_SIZE;
 
@@ -136,8 +181,11 @@ char *diskGetMounted(struct paramInfo * pi) {
 	}
 
 	k = 0;
-
+#if defined(__FreeBSD__) || (__OpenBSD__)
+	if((j = getmntinfo(&newStat, MNT_NOWAIT)) != 0) {
+#elif defined(__NetBSD__)
 	if((j = getmntinfo(&newStat, ST_NOWAIT)) != 0) {
+#endif
 		for(i = 0; i < j; i++) {
 			if(newStat[i].f_mntfromname[0] == 0 || newStat[i].f_mntonname[0] == 0 || newStat[i].f_fstypename[0] == 0) {
 				continue;
@@ -163,22 +211,38 @@ char *diskGetMounted(struct paramInfo * pi) {
 			sysFree = 0;
 			sysSerial = 0;
 
-			sysMultiplier = 0;
-
+			p = 0;
+#if defined(__FreeBSD__) || (__OpenBSD__)
 			if(newStat[i].f_bsize > 0) {
-				sysMultiplier = newStat[i].f_bsize;
+				p = newStat[i].f_bsize;
 			}
-
-			sysSize = (newStat[i].f_blocks * sysMultiplier) / 10000;
-			sysFree = (newStat[i].f_bfree * sysMultiplier) / 10000;
+#elif defined(__NetBSD__)
+			if(newStat[i].f_frsize > 0) {
+				p = newStat[i].f_frsize;
+			}
+#endif
+			sysSize = (newStat[i].f_blocks * p) / 1000;
+			sysFree = (newStat[i].f_bfree * p) / 1000;
+#if defined(__FreeBSD__) || (__OpenBSD__)
+			sysSerial = newStat[i].f_fsid.val[0];
+			sysSerial += newStat[i].f_fsid.val[1];
+			sysSerial += newStat[i].f_fsid.val[2];
+#elif defined(__NetBSD__)
 			sysSerial = newStat[i].f_fsid;
-
+#endif
 			memset(newOptions, 0, sizeof(newOptions));
 
 			g = 1;
-
+#if defined(__FreeBSD__)
+			for(l = 0; l < sizeof(uint64_t) * 8; l++) {
+				f = newStat[i].f_flags;
+#elif defined(__NetBSD__)
 			for(l = 0; l < sizeof(unsigned long) * 8; l++) {
 				f = newStat[i].f_flag;
+#elif defined(__OpenBSD__)
+			for(l = 0; l < sizeof(u_int32_t) * 8; l++) {
+				f = newStat[i].f_flags;
+#endif
 				f &= g;
 
 				for(m = 0; ; m++) {
